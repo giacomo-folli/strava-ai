cd6e3b912e07a80c99be98c7ca9f75cd1c4aaa4f
Giacomo <giacomo.folli@studenti.unipr.it>
2025-05-15 18:04:36 +0200

    style: format code for improved readability and consistency

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,55 +1,70 @@
 <script lang="ts">
-    import { page } from "$app/state";
+  import { page } from "$app/state";
 </script>
 
 <svelte:head>
-    <title>Strava Connection</title>
+  <title>Strava Connection</title>
 </svelte:head>
 
 <main class="container">
-    <slot />
+  <slot />
 
-    {#if !page?.route?.id?.includes("review") && !page?.route?.id?.includes("confirmation")}
-        <div class="logo">
-            <img width="100px" src="/api_logo_cpt.png" alt="api-logo" />
-        </div>
-    {/if}
+  {#if !page?.route?.id?.includes("review") && !page?.route?.id?.includes("confirmation")}
+    <div class="logo">
+      <img width="100px" src="/api_logo_cpt.png" alt="api-logo" />
+    </div>
+
+    <div class="changelog">
+      <a href="https://strava.openchangelog.com"> changelog </a>
+    </div>
+  {/if}
 </main>
 
 <style>
-    :global(*) {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
+  :global(*) {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-    :global(html, body) {
-        height: 100%;
-        overflow: hidden;
-    }
+  :global(html, body) {
+    height: 100%;
+    overflow: hidden;
+  }
 
-    .container {
-        display: flex;
-        position: relative;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 100vh;
-        font-family: system-ui, sans-serif;
-        padding: 20px;
-        box-sizing: border-box;
-    }
+  .container {
+    display: flex;
+    position: relative;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    font-family: system-ui, sans-serif;
+    padding: 20px;
+    box-sizing: border-box;
+  }
 
-    .logo {
-        position: absolute;
-        bottom: 20px;
-        left: 25px;
-    }
+  .logo {
+    position: absolute;
+    bottom: 20px;
+    left: 25px;
+  }
 
-    :global(h1) {
-        margin-bottom: 2rem;
-        text-align: center;
-        max-width: 600px;
-        padding: 0 1rem;
-    }
+  .changelog {
+    position: absolute;
+    top: 15px;
+    left: 25px;
+  }
+
+  .changelog a {
+    color: #fc4c02;
+    font-size: 12px;
+  }
+
+  :global(h1) {
+    margin-bottom: 2rem;
+    text-align: center;
+    max-width: 600px;
+    padding: 0 1rem;
+  }
 </style>
